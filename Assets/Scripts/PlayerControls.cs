ca8d6b3ce62ab3a34605b6eef5246b02b5505132
juyonLee00 <ljy012900@gmail.com>
2023-01-28 17:38:20 +0900

    이전 Input System 사용해서 방향키로 플레이어 이동

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,23 +6,23 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
+    [SerializeField] float xControlSpeed = 30f;
+    [SerializeField] float yControlSpeed = 20f;
 
-    void OnEnable() 
-    {
-        movement.Enable();
-    }
-
-    void OnDisable() 
-    {
-        movement.Disable();
-    }
-    
     void Update()
     {
-        float horizontalThrow = movement.ReadValue<Vector2>().x;
-        float verticalThrow = movement.ReadValue<Vector2>().y;
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+        
+        float xOffset = xThrow * Time.deltaTime * xControlSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
 
-        Debug.Log(horizontalThrow);
-        Debug.Log(verticalThrow);
+        float yOffset = yThrow * Time.deltaTime * yControlSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3
+        (newXPos, 
+        newYPos,
+        transform.localPosition.z);
     }
 }
